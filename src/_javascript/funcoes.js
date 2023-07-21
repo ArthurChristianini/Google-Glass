6216fc4162464eba42afa7f04fb35d386e00013e
ArthurChristianini <139179287+ArthurChristianini@users.noreply.github.com>
2023-07-21 16:02:55 -0300

    Add files via upload

---
+++ src/_javascript/funcoes.js
@@ -0,0 +1,9 @@
+<script type="text/javascript">
+
+    function calc(){
+        var qtd = parseInt(document.getElementById('cQtd').value);
+        var tot = qtd * 1500;
+        document.getElementById('cTot').value = tot
+    }
+
+</script>
